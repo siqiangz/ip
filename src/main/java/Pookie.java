eb716ec8730f25c497fa009c617f41c65d592898
siqiangz <szhongq21@gmail.com>
2025-01-26 20:44:13 +0800

    Level 0: Completed. Renamed bot to Pookie

---
+++ src/main/java/Pookie.java
@@ -0,0 +1,21 @@
+public class Pookie {
+    public static void main(String[] args) {
+        greeting();
+        //System.out.println("Hello from\n" + logo);
+    }
+
+    public static void greeting() {
+        String logo = "__________              __   .__        \n"
+                 + "\\______   \\____   ____ |  | _|__| ____  \n"
+                 + " |     ___/  _ \\ /  _ \\|  |/ /  |/ __ \\ \n"
+                 + " |    |  (  <_> |  <_> )    <|  \\  ___/ \n"
+                 + " |____|   \\____/ \\____/|__|_ \\__|\\___>\n";
+        System.out.println("** ** ** ** ** Hello from ** ** ** ** **\n" + logo);
+        System.out.println("_________________________________________");
+        System.out.println("Hello! I'm Pookie, your task tracker!");
+        System.out.println("What can I do for you?");
+        System.out.println("_________________________________________");
+        System.out.println("Bye bye, hope to see you again soon!");
+        System.out.println("_________________________________________");
+    }
+}
