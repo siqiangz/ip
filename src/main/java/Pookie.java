d636750fcb6186ecea8bcc0bdf3bb36be6c4f167
siqiangz <szhongq21@gmail.com>
2025-01-28 16:51:38 +0800

    Level 1: Added doEcho(). Indentations added.

--- src/main/java/Pookie.java
+++ src/main/java/Pookie.java
@@ -1,20 +1,57 @@
+import java.util.Scanner;
+
 public class Pookie {
-    public static void main(String[] args) {
-        greeting();
-    }
+    private static boolean onOffSwitch = true;
 
     public static void greeting() {
-        String logo = "__________              __   .__        \n"
-                 + "\\______   \\____   ____ |  | _|__| ____  \n"
-                 + " |     ___/  _ \\ /  _ \\|  |/ /  |/ __ \\ \n"
-                 + " |    |  (  <_> |  <_> )    <|  \\  ___/ \n"
-                 + " |____|   \\____/ \\____/|__|_ \\__|\\___>\n";
-        System.out.println("** ** ** ** ** Hello from ** ** ** ** **\n" + logo);
-        System.out.println("_________________________________________");
-        System.out.println("Hello! I'm Pookie, your task tracker!");
-        System.out.println("What can I do for you?");
-        System.out.println("_________________________________________");
-        System.out.println("Bye bye, hope to see you again soon!");
-        System.out.println("_________________________________________");
+        String logo = "\t__________              __   .__        \n"
+                + "\t\\______   \\____   ____ |  | _|__| ____  \n"
+                + "\t |     ___/  _ \\ /  _ \\|  |/ /  |/ __ \\ \n"
+                + "\t |    |  (  <_> |  <_> )    <|  \\  ___/ \n"
+                + "\t |____|   \\____/ \\____/|__|_ \\__|\\___>\n";
+        System.out.println("\t** ** ** ** ** Hello from ** ** ** ** **\n" + logo);
+        doLineBreak();
+        System.out.println("\tHello! I'm Pookie, your task tracker!");
+        System.out.println("\tWhat can I do for you?");
+        doLineBreak();
     }
+
+    public static void goodbye() {
+        System.out.println("\tBye bye, hope to see you again soon!");
+        doLineBreak();
+    }
+
+    public static void doEcho(String sentence) {
+        System.out.println("\t" + sentence);
+        doLineBreak();
+    }
+
+    public static void doLineBreak() {
+        System.out.println("\t_________________________________________");
+    }
+
+    public static void commandDictionary(String lineInput) {
+        switch (lineInput) {
+        case "bye":
+            goodbye();
+            onOffSwitch = false;
+            break;
+        default:
+            doEcho(lineInput);
+            break;
+        }
+    }
+
+    public static void main(String[] args) {
+        Scanner in = new Scanner(System.in);
+        String lineInput;
+        greeting();
+        while (onOffSwitch) {
+            lineInput = in.nextLine();
+            doLineBreak();
+            commandDictionary(lineInput);
+        }
+    }
+
+
 }
